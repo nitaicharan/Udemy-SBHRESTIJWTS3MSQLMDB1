7ea3a9461c2d0aebf48f3f295faeee4a36fa9495
Nitai Charan <nitaicharan@gmail.com>
2020-06-02 00:00:00 -0300

    10. Nivelamento sobre JPA - PARTE 3 (AULA EXTRA)

--- src/main/java/com/course/CourseApplication.java
+++ src/main/java/com/course/CourseApplication.java
@@ -1,19 +1,42 @@
 package com.course;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import com.course.dominio.Pessoa;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CourseApplication {
 
 	public static void main(String[] args) {
-		Pessoa p1 = new Pessoa(1, "Carlos da Silva", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(2, "Joaquim Torres", "joaquin@gmail.com");
-		Pessoa p3 = new Pessoa(3, "Ana Maria", "ana@gmail.com");
-		
-		SpringApplication.run(CourseApplication.class, args);
+		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
+		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquin@gmail.com");
+		Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+
+		Pessoa pA = em.find(Pessoa.class, 2);
+		Pessoa pB = new Pessoa(null, "Joaquim Torres", "joaquin@gmail.com");
+
+		em.remove(pB); // Detached entity - Do not remove
+
+		em.getTransaction().begin(); // When a operation different from a query needs to start transaction
+		em.remove(pA); // Monitored entity - remove
+		em.getTransaction().commit();
+
+		em.getTransaction().commit();
+		System.out.println("Pronto!");
+
+		em.close();
+		emf.close();
 	}
 
 }
--- src/main/java/com/course/dominio/Pessoa.java
+++ src/main/java/com/course/dominio/Pessoa.java
@@ -2,6 +2,11 @@ package com.course.dominio;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,11 +16,14 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@Entity
 @NoArgsConstructor
 @AllArgsConstructor
 public class Pessoa implements Serializable{
     private static final long serialVersionUID = 1L;
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     private String email;
