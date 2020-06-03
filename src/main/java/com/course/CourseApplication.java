695eaeaf1fb40543d6c1fadc1b14f4eddc187b29
Nitai Charan <nitaicharan@gmail.com>
2020-06-03 00:00:00 -0300

    9. Nivelamento sobre JPA - PARTE 2 (AULA EXTRA)

--- src/main/java/com/course/CourseApplication.java
+++ src/main/java/com/course/CourseApplication.java
@@ -1,5 +1,7 @@
 package com.course;
 
+import com.course.dominio.Pessoa;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
